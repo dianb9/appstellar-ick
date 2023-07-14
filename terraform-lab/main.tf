a65623c260e274f1ee48c109459d91d3f5af8970
unknown <ardianbicaj9@hotmail.com>
2023-07-14 20:52:05 +0200

    added s3 bucket

--- terraform-lab/main.tf
+++ terraform-lab/main.tf
@@ -6,6 +6,15 @@ data "aws_vpc" "emri" {
 
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-ardian-ick"
+
+  tags = {
+    Name        = "Ardian ICK"
+    Environment = "Dev"
+  }
+}
+
 output "vpc_id" {
 
     value = data.aws_vpc.emri.id
